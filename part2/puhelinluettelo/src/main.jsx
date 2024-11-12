157a0f08e42bc1192927b89585ab36f521c60f04
Roope Paukku <roope.paukku@gmail.com>
2024-11-12 23:48:24 +0200

    Ex 2.11.

--- part2/puhelinluettelo/src/main.jsx
+++ part2/puhelinluettelo/src/main.jsx
@@ -1,6 +1,13 @@
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
+import axios from 'axios'
+
+axios
+  .get('http://localhost:3001/persons')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+  })
 
 const notes = [
   {
