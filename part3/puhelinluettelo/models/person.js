e8cb6ee4b15493af0cdfc3b6d31a243e9d991e98
Roope Paukku <roope.paukku@gmail.com>
2025-10-12 19:40:50 +0300

    3.20.

--- part3/puhelinluettelo/models/person.js
+++ part3/puhelinluettelo/models/person.js
@@ -22,7 +22,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number! Format should be XX-XXXXXXX or XXX-XXXXXXX`
+    }
   },
 })
 
